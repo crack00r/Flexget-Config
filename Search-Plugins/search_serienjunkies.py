8704a44247dc6e2e34a0b8d1c7ae00a9045dd5b0
CENGOiSM <mizaras29@hotmail.de>
2016-02-27 14:43:34 +0100

    Hoster- und Sprachauswahl ergänzt
    - searchSerienjunkies:
      hoster: uploaded.to
      language: german
    
    so wird es verwendet

--- Search-Plugins/search_serienjunkies.py
+++ Search-Plugins/search_serienjunkies.py
@@ -14,56 +14,45 @@ from flexget.utils.search import torrent_availability, normalize_unicode
 
 log = logging.getLogger('searchSerienjunkies')
 
-CATEGORIES = {
-    # Hoster
-    'Uploaded': "ul",
-    'ShareOnline': "so",
+HOSTER = ['uploaded.to', 'share-online.biz']
+LANGUAGE = ['english', 'german']
 
-    # Movies
-    'BluRay 720p': 2,
-    'BluRay 1080p': 5,
-    'XviD': 15,
-    'BRRip': 16,
+DEFHOS = 'uploaded.to'
+DEFLANG = 'german'
 
-    # TV
-    'HDTV': 7,
-    'SDTV': 24,
-    'TV WEB-DL': 14
-}
+#CATEGORIES = {
+#    # Hoster
+#    'Uploaded': "ul",
+#    'ShareOnline': "so",
+#
+#    # Sprache
+#    'Sprache': "deutsch",
+#
+#    # Movies
+#    'BluRay 720p': 2,
+#    'BluRay 1080p': 5,
+#    'XviD': 15,
+#    'BRRip': 16,
+#
+#    # TV
+#    'HDTV': 7,
+#    'SDTV': 24,
+#    'TV WEB-DL': 14
+#}
 
 
 class SearchSerienjunkies(object):
     """
         Serienjunkies search plugin.
-
-        To perform search against single category:
-
-        publichd:
-            category: BluRay 720p
-
-        To perform search against multiple categories:
-
-        publichd:
-            category:
-                - BluRay 720p
-                - BluRay 1080p
-
-        Movie categories accepted: BluRay 720p, BluRay 1080p, XviD, BRRip
-        TV categories accepted: HDTV, SDTV, TV WEB-DL
-
-        You can use also use category ID manually if you so desire (eg. BluRay 720p is actually category id '2')
     """
 
     schema = {
         'type': 'object',
         'properties': {
-            'category': one_or_more({
-                'oneOf': [
-                    {'type': 'integer'},
-                    {'type': 'string', 'enum': list(CATEGORIES)},
-                ]})
+            'language': {'type': 'string', 'enum': LANGUAGE},
+            'hoster': {'type': 'string', 'enum': HOSTER}
         },
-        "additionalProperties": False
+        'additionalProperties': False
     }
 
     @plugin.internet(log)
@@ -71,17 +60,11 @@ class SearchSerienjunkies(object):
         """
             Search for entries on Serienjunkies
         """
-
-#        categories = config.get('category', 'all')
-        # Ensure categories a list
-#        if not isinstance(categories, list):
-#            categories = [categories]
-        # Convert named category to its respective category id number
-#        categories = [c if isinstance(c, int) else CATEGORIES[c] for c in categories]
-#        category_url_fragment = '&category=%s' % urllib.quote(';'.join(str(c) for c in categories))
-
         base_url = 'http://serienjunkies.org/search/'
         mull = {"Dauer:", "Download:", "Uploader:", u"Größe:", u"Tonhöhe:", "Sprache:", "Format:", "HQ-Cover:"}
+        self.config = task.config.get('searchSerienjunkies') or {}
+        self.config.setdefault('hoster', DEFHOS)
+        self.config.setdefault('language', DEFLANG)
 
         entries = set()
         for search_string in entry.get('search_strings', [entry['title']]):
@@ -94,17 +77,32 @@ class SearchSerienjunkies(object):
 
             page = requests.get(url).content
             soup = get_soup(page)
-            hoster = 'uploaded.to' # In "share-online.biz" ändern für anderen Hoster. Wird integriert in Flexget via config.yml. Im Moment nur manuell einstellbar
+            hoster = self.config['hoster']
+            if self.config['language'] == 'english':
+                english = True
+            else:
+                english = None
             for p in soup.find_all('p'):
                 entry = Entry()
                 if p.strong is not None and p.strong.text not in mull:
+                  if english:
                     try:
+                      if not p.strong.find(text=re.compile("german", flags=re.IGNORECASE)):
                         link = p.find(text=re.compile(hoster)).find_previous('a')
                         entry['title'] = p.strong.text
                         entry['url'] = link.get('href')
                         entries.add(entry)
                     except:
-                        pass
+                      pass
+                  else:
+                    try:
+                      if p.strong.find(text=re.compile("german", flags=re.IGNORECASE)):
+                        link = p.find(text=re.compile(hoster)).find_previous('a')
+                        entry['title'] = p.strong.text
+                        entry['url'] = link.get('href')
+                        entries.add(entry)
+                    except:
+                      pass
         return entries
 
 
